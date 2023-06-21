eaec074d273a9540d3ced591be1628ce9beb5a7b
Ravaliganigapeta <130034674+Ravaliganigapeta@users.noreply.github.com>
2023-06-21 12:39:42 +0530

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,15 @@
+resource "aws_s3_bucket" "Tf-bucket" {
+  bucket = "first-bucket06" // Bucket name must be globally unique and must not contain spaces or uppercase letters. Name of the bucket. If omitted,Terraform will assign a random, unique name.
+  acl    = "private" // Each ACL has a predefined set of grantees and permissions.
+
+ 
+  versioning {
+     enabled =  true // You can use S3 Versioning to keep multiple versions of an object in one bucket
+   }
+
+  tags = {
+    Name = "My-Bucket",
+    Service = "devops"
+    Environment = "Dev"
+  }
+}
