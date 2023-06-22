a47f17c68eca1643ef7d2ddb2bc290b6c9f2b684
Ravaliganigapeta <130034674+Ravaliganigapeta@users.noreply.github.com>
2023-06-22 14:20:53 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "Tf-bucket" {
-  bucket = "bucket1505" // Bucket name must be globally unique and must not contain spaces or uppercase letters. Name of the bucket. If omitted,Terraform will assign a random, unique name.
+  bucket = "first-bucket1505" // Bucket name must be globally unique and must not contain spaces or uppercase letters. Name of the bucket. If omitted,Terraform will assign a random, unique name.
   acl    = "private" // Each ACL has a predefined set of grantees and permissions.
 
  
